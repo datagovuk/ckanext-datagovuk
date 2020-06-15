465c70f443e2600adc41154a98c3b5c118056eec
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2020-06-15 14:26:12 +0100

    TMP2

--- bin/jenkins-tests.sh
+++ bin/jenkins-tests.sh
@@ -7,6 +7,7 @@ venv/bin/pip install -r dev-requirements.txt
 dropdb ckan_test; createdb ckan_test
 venv/bin/python setup.py develop
 echo $PATH
+which paster
 paster --plugin=ckan db init -c test-jenkins.ini
 paster --plugin=ckanext-harvest harvester initdb -c test-jenkins.ini
 
