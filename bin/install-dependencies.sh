92aa666ac2ea2f7415dba9f1c8be3158874897c1
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2020-06-15 15:14:19 +0100

    TMP3

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -9,6 +9,7 @@ ckan_dcat_sha='b757e5be643a17f08b1bb102348c370abee149d5'
 ckan_spatial_fork='alphagov'
 ckan_spatial_sha='c35e1249cfb3c7b2f1286f433fd51c6fe8f05b72'
 
+ckan_fork='alphagov'
 ckan_sha='ckan-2.8.3-dgu'
 
 pycsw_tag='2.4.0'
@@ -24,9 +25,9 @@ $pip install -U "git+https://github.com/ckan/ckanext-dcat.git@$ckan_dcat_sha#egg
 $pip install -U $(curl -s https://raw.githubusercontent.com/$ckan_spatial_fork/ckanext-spatial/$ckan_spatial_sha/pip-requirements.txt)
 $pip install -U "git+https://github.com/$ckan_spatial_fork/ckanext-spatial.git@$ckan_spatial_sha#egg=ckanext-spatial"
 
-$pip install -U $(curl -s https://raw.githubusercontent.com/ckan/ckan/$ckan_sha/requirement-setuptools.txt)
-$pip install -r https://raw.githubusercontent.com/ckan/ckan/$ckan_sha/requirements.txt
-$pip install -Ue "git+https://github.com/ckan/ckan.git@$ckan_sha#egg=ckan"
+$pip install -U $(curl -s https://raw.githubusercontent.com/$ckan_fork/ckan/$ckan_sha/requirement-setuptools.txt)
+$pip install -r https://raw.githubusercontent.com/$ckan_fork/ckan/$ckan_sha/requirements.txt
+$pip install -Ue "git+https://github.com/$ckan_fork/ckan.git@$ckan_sha#egg=ckan"
 
 $pip install -r requirements.txt
 
