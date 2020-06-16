60c57cb430807857348071a06136f66393b156aa
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2020-06-16 13:10:02 +0100

    TMP6

--- bin/jenkins-tests.sh
+++ bin/jenkins-tests.sh
@@ -7,7 +7,7 @@ venv/bin/pip install -r dev-requirements.txt
 dropdb ckan_test; createdb ckan_test
 venv/bin/python setup.py develop
 
-curl http://127.0.0.1:8983/solr/admin/info/system?wt=json
+curl http://127.0.0.1:8983/solr/admin/cores?action=STATUS
 
 paster --plugin=ckan db init -c test-jenkins.ini
 paster --plugin=ckanext-harvest harvester initdb -c test-jenkins.ini
